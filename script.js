49409c7516df6b3536f5b385d9e30421c8be8c21
Elizeu Junior <77870830+ermyjr@users.noreply.github.com>
2022-07-26 23:30:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('convert-button')
+
+const dolar = 5.2
+
+
+const convertValues = () => {
+    const inputReal = document.getElementById('input-real').value 
+
+    inputReal/dolar
+
+
+ }
+
+button.addEventListener('click',convertValues)
